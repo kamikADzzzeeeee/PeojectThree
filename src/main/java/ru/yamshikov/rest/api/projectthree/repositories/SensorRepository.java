52527b36fe5dae9651df4ef3685ceb9b23c0c390
kamikADzzzeeeee <yamshikov.denis2016@yandex.ru>
2023-11-20 14:13:19 +0500

    Добавил в Service и в Repository метод для поиска датчика по серийному номеру

--- src/main/java/ru/yamshikov/rest/api/projectthree/repositories/SensorRepository.java
+++ src/main/java/ru/yamshikov/rest/api/projectthree/repositories/SensorRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.yamshikov.rest.api.projectthree.models.Sensor;
 
+import java.util.Optional;
+
 @Repository
 public interface SensorRepository extends JpaRepository<Sensor, Integer> {
+
+
+    public Optional<Sensor> findBySerialNumber(long serialNumber);
+
+
 }
--- src/main/java/ru/yamshikov/rest/api/projectthree/services/SensorService.java
+++ src/main/java/ru/yamshikov/rest/api/projectthree/services/SensorService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.yamshikov.rest.api.projectthree.models.Sensor;
 import ru.yamshikov.rest.api.projectthree.repositories.SensorRepository;
+import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.SensorAlreadyExistsException;
 
 import java.util.List;
 
@@ -18,4 +19,14 @@ public class SensorService {
     public List<Sensor> findAllSensors(){
         return sensorRepository.findAll();
     }
+
+    @Transactional(readOnly = false)
+    public void creatSensor(Sensor sensor){
+        sensorRepository.findBySerialNumber(sensor.getSerialNumber()).ifPresent(action ->{
+            throw new SensorAlreadyExistsException();
+        });
+        sensorRepository.save(sensor);
+    }
+
+
 }
