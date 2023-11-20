8512cabbe1bfb867e49d0143c05fcd51ad10692e
kamikADzzzeeeee <yamshikov.denis2016@yandex.ru>
2023-11-21 01:20:08 +0500

    Обновил в связи с изменение ErrorResponse

--- src/main/java/ru/yamshikov/rest/api/projectthree/util/errors/CustomControllerAdvice.java
+++ src/main/java/ru/yamshikov/rest/api/projectthree/util/errors/CustomControllerAdvice.java
@@ -4,9 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.EmptySensorListException;
-import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.SensorAlreadyExistsException;
-import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.SensorNotCreatedException;
+import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.measurement.EmptyMeasurementListException;
+import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.measurement.MeasurementNotRegistratedException;
+import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.sensor.EmptySensorListException;
+import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.sensor.SensorAlreadyExistsException;
+import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.sensor.SensorNotCreatedException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,23 +16,23 @@ import java.time.format.DateTimeFormatter;
 @ControllerAdvice
 public class CustomControllerAdvice {
     @ExceptionHandler(EmptySensorListException.class)
-    public ResponseEntity<SensorErrorResponse> handleException(EmptySensorListException e) {
+    public ResponseEntity<ErrorResponse> handleException(EmptySensorListException e) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-        SensorErrorResponse response = new SensorErrorResponse("Список датчиков пуст", dtf.format(LocalDateTime.now()));
+        ErrorResponse response = new ErrorResponse("Список датчиков пуст", dtf.format(LocalDateTime.now()));
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(SensorNotCreatedException.class)
-    public ResponseEntity<SensorErrorResponse> handleException(SensorNotCreatedException e) {
+    public ResponseEntity<ErrorResponse> handleException(SensorNotCreatedException e) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-        SensorErrorResponse response = new SensorErrorResponse(e.getMessage(), dtf.format(LocalDateTime.now()));
+        ErrorResponse response = new ErrorResponse(e.getMessage(), dtf.format(LocalDateTime.now()));
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(SensorAlreadyExistsException.class)
-    public ResponseEntity<SensorErrorResponse> handleException(SensorAlreadyExistsException e) {
+    public ResponseEntity<ErrorResponse> handleException(SensorAlreadyExistsException e) {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-        SensorErrorResponse response = new SensorErrorResponse("Датчик с таким серйным номером уже существует", dtf.format(LocalDateTime.now()));
+        ErrorResponse response = new ErrorResponse("Датчик с таким серйным номером уже существует", dtf.format(LocalDateTime.now()));
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
