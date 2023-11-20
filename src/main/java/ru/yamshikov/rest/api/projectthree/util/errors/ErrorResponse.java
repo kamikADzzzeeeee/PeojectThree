559169211adc57fb65a1a902e0238fda9eb2eea5
kamikADzzzeeeee <yamshikov.denis2016@yandex.ru>
2023-11-21 01:18:40 +0500

    Обновил название ErrorResponse

--- src/main/java/ru/yamshikov/rest/api/projectthree/util/errors/ErrorResponse.java
+++ src/main/java/ru/yamshikov/rest/api/projectthree/util/errors/ErrorResponse.java
@@ -3,14 +3,11 @@ package ru.yamshikov.rest.api.projectthree.util.errors;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 @Setter
-public class SensorErrorResponse{
+public class ErrorResponse {
     private String message;
     private String timestamp;
 }
