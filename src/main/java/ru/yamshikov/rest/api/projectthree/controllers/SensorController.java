e26ee4199916b54d0a5973764a430f93a9db4468
kamikADzzzeeeee <yamshikov.denis2016@yandex.ru>
2023-11-20 14:15:09 +0500

    Добавил метод POST в RestController для добавления новых датчиков в БД

--- src/main/java/ru/yamshikov/rest/api/projectthree/controllers/SensorController.java
+++ src/main/java/ru/yamshikov/rest/api/projectthree/controllers/SensorController.java
@@ -1,18 +1,19 @@
 package ru.yamshikov.rest.api.projectthree.controllers;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.yamshikov.rest.api.projectthree.dto.sensor.SensorDtoInfo;
-import ru.yamshikov.rest.api.projectthree.mapper.sensor.SensorMapperInfo;
-import ru.yamshikov.rest.api.projectthree.models.Sensor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import ru.yamshikov.rest.api.projectthree.dto.SensorDtoInfo;
+import ru.yamshikov.rest.api.projectthree.mapper.SensorMapperInfo;
 import ru.yamshikov.rest.api.projectthree.services.SensorService;
 import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.EmptySensorListException;
+import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.SensorNotCreatedException;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,6 +36,24 @@ public class SensorController {
         } else {
             return new ResponseEntity<>(sensors, HttpStatus.OK);
         }
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<HttpStatus> registrationPage(@RequestBody @Valid SensorDtoInfo dtoInfo,
+                                                       BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            StringBuilder result = new StringBuilder();
+            for (FieldError error : errors){
+                result.append(error.getField())
+                      .append(" - ")
+                      .append(error.getDefaultMessage())
+                      .append(";");
+            }
+            throw new SensorNotCreatedException(result.toString());
+        }
+        sensorService.creatSensor(sensorMapperInfo.toEntity(dtoInfo));
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
