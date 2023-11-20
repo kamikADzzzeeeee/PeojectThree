d80e986e8b97f66343a0f25b2d3fd597565434cf
kamikADzzzeeeee <yamshikov.denis2016@yandex.ru>
2023-11-21 01:23:57 +0500

    Revert "Убрал из контроллера датчика некоторый функционал в сервис датчика"
    This reverts commit edd34aff94e9a2542825f5c65ab2de77a84078e6.

--- src/main/java/ru/yamshikov/rest/api/projectthree/controllers/SensorController.java
+++ src/main/java/ru/yamshikov/rest/api/projectthree/controllers/SensorController.java
@@ -7,11 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-import ru.yamshikov.rest.api.projectthree.dto.SensorDtoInOut;
-import ru.yamshikov.rest.api.projectthree.mapper.SensorMapper;
+import ru.yamshikov.rest.api.projectthree.dto.SensorDtoInfo;
+import ru.yamshikov.rest.api.projectthree.mapper.SensorMapperInfo;
 import ru.yamshikov.rest.api.projectthree.services.SensorService;
-import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.sensor.SensorNotCreatedException;
+import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.EmptySensorListException;
+import ru.yamshikov.rest.api.projectthree.util.errors.exceptions.SensorNotCreatedException;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,19 +23,24 @@ import java.util.stream.Collectors;
 public class SensorController {
 
     private final SensorService sensorService;
-    private final SensorMapper sensorMapper;
+    private final SensorMapperInfo sensorMapperInfo;
 
     @GetMapping()
-    public ResponseEntity<List<SensorDtoInOut>> sensorsPage(){
-        List<SensorDtoInOut> sensors = sensorService.findAllSensors()
+    public ResponseEntity<List<SensorDtoInfo>> sensorsPage(){
+        List<SensorDtoInfo> sensors = sensorService.findAllSensors()
                 .stream()
-                .map(sensorMapper::toDto)
+                .map(sensorMapperInfo::toDto)
                 .collect(Collectors.toList());
-        return new ResponseEntity<>(sensors, HttpStatus.OK);
+        if (sensors.isEmpty()){
+            throw new EmptySensorListException();
+        } else {
+            return new ResponseEntity<>(sensors, HttpStatus.OK);
+        }
     }
 
     @PostMapping("/registration")
-    public ResponseEntity<HttpStatus> registrationPage(@RequestBody @Valid SensorDtoInOut dtoInfo, BindingResult bindingResult){
+    public ResponseEntity<HttpStatus> registrationPage(@RequestBody @Valid SensorDtoInfo dtoInfo,
+                                                       BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             List<FieldError> errors = bindingResult.getFieldErrors();
             StringBuilder result = new StringBuilder();
@@ -41,11 +48,11 @@ public class SensorController {
                 result.append(error.getField())
                       .append(" - ")
                       .append(error.getDefaultMessage())
-                      .append("; ");
+                      .append(";");
             }
             throw new SensorNotCreatedException(result.toString());
         }
-        sensorService.creatSensor(sensorMapper.toEntity(dtoInfo));
+        sensorService.creatSensor(sensorMapperInfo.toEntity(dtoInfo));
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
