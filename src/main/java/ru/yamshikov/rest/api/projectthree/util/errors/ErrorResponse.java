f62a76e9869ed3cab5779cbc37151867436fa36f
kamikADzzzeeeee <yamshikov.denis2016@yandex.ru>
2023-11-29 16:04:58 +0500

    Добавил для сущности ошибки новое поле

--- src/main/java/ru/yamshikov/rest/api/projectthree/util/errors/ErrorResponse.java
+++ src/main/java/ru/yamshikov/rest/api/projectthree/util/errors/ErrorResponse.java
@@ -1,13 +1,33 @@
 package ru.yamshikov.rest.api.projectthree.util.errors;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
+import java.util.List;
+
+
+@Schema(description = "Транспортная сущность информации при получении ошибки")
+@Data
 @AllArgsConstructor
-@Getter
-@Setter
+
 public class ErrorResponse {
+    @Schema(description = "Сообщение об ошибке",
+            example = "Ошибка по причине ...")
+
     private String message;
+
+    @Schema(description = "Список ошибок")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<String> errors;
+
+    @Schema(description = "Дата/время",
+            example = "23.11.2023 00:23:05")
     private String timestamp;
+
+    public ErrorResponse(String message, String timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
 }
